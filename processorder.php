b470f3fae44cb450a000eebd2e5fdc4f7f7e013a
Miller <nmiller2379@gmail.com>
2024-02-11 11:33:44 -0500

    initial commit

---
+++ processorder.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bob's Auto Parts - Order Results</title>
+</head>
+<body>
+    <h1>Bob's Auto Parts</h1>
+    <h2>Order Results</h2>
+    <?php
+    // Using echo to print statements to the browser
+        echo "<p>Order processed at ";
+        // Using the date function to print the current date and time
+        echo date('g:i A T, F jS, Y.');
+        echo "</p>";
+        ?>
+</body>
+</html>
